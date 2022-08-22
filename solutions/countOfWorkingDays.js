38315a7972904563f56009e5684361d8063b2558
annivadas <anni.vadas@gmail.com>
2022-08-22 14:48:34 +0200

    1.feladat kész

--- solutions/countOfWorkingDays.js
+++ solutions/countOfWorkingDays.js
@@ -1,1 +1,19 @@
+/*
+2. Írj egy függvényt `countOfWorkingDays` néven, ami két paramétert kap. Mind a két paraméter egy-egy dátum. Az első a  korábbi időpont, a második  a későbbi időpont.  A visszatérési értéke a két dátum közötti munkanapok (hétfő-péntek) száma! A különböző ünnepekkel nem kell számolnod! Ha az első dátum korábbi, mint a második akkor dobj egy hibát az alábbi üzenettel: `The first parameter is earlier date, than second!`!
+Hibát dobni a következő módon tudsz: 
+
+```javascript
+  throw new Error(message)
+```
+A aktuális napot is bele kell számolni a munkanapokba, ha az munkanap!
+Sehol ne használj kerekítést!
+ */
+const countOfWorkingDays = (date1 = new Date(), date2 = new Date()) => {
+  
+}
 export default countOfWorkingDays;
+
+
+
+
+
